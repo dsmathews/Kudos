cb3cc0407854065ea45d53705c058b8380c42d44
Darrell Mathews <smathewsbd@gmail.com>
2018-11-27 09:47:05 -0500

    changed app and api-routes to fix problems with rendering to index.html

--- models/kudos.js
+++ models/kudos.js
@@ -2,8 +2,20 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 var KudosSchema = new Schema({
-  title: String,
-  body: String
+  title: {
+    type: String
+  }, 
+  body: {
+    type: String
+  },
+  from: {
+    type: String
+  },
+  to: {
+    type: String
+  }
+  
+  
 });
 
 const kudos = mongoose.model("kudos", KudosSchema);
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,10 @@
 const User = require('../models/user');
-const Note = require('../models/kudos');
+const kudos = require('../models/kudos');
 
 module.exports = function (app) {
 
-  app.post('/api/user', function (req, res) {
-    User.find(req.body)
+  app.get('/api/user', function (req, res) {
+    User.find()
     .then(function (data) {
       res.json(data);
     })
@@ -34,16 +34,28 @@ module.exports = function (app) {
     });
   });
 
+  app.get('/api/kudos', function (req, res) {
+    kudos.find()
+    .then(function (data) {
+      res.json(data);
+    })
+    .catch(function (err) {
+      res.json(err);
+    });
+  });
+
   app.post('/api/kudos', function (req, res) {
     const userId = req.body.userId;
     const newEntry = {
       title: req.body.title,
-      body: req.body.body
+      body: req.body.body,
+      from: req.body.from,
+      to: req.body.to
     };
-
-    Note.create(newEntry)
-      .then(function (noteData) {
-      return User.findOneAndUpdate({_id: userId}, { $push: { notes: noteData._id } }, { new: true });
+    console.log(newEntry);
+    kudos.create(newEntry)
+      .then(function (kudosData) {
+      return user.findOneAndUpdate({_id: userId}, { $push: { kudos: kudosData._id } }, { new: true });
     })
     .then(function(userData) {
       res.json(userData);
