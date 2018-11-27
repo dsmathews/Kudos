5ddc04f1b01af3a80732ddf4a9896437c99e8f73
Darrell Mathews <smathewsbd@gmail.com>
2018-11-27 15:12:51 -0500

    Update to server

--- server.js
+++ server.js
@@ -9,9 +9,24 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost/kudos', {useNewUrlParser: true});
+const databaseUri = 'mongodb://localhost/kudos';
 
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI)
+} else {
+    mongoose.connect(databaseUri, {useNewUrlParser: true});
+}
+
+const db = mongoose.connection;
+db.on('error', function (err){
+    console.log("Mongoose Error: ", err);
+});
+db.once('open', function () {
+    console.log("Mongoose connection successful.");
+});
 require('./routes/api-routes')(app);
+require('./models/kudos.js');
+require('./models/user.js');
 
 app.listen(PORT, function(){
     console.log(`App is listening on port ${PORT}`);
